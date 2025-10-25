81cd506404bd3551930b21b8e8c8897c346e10cb
rawbytedev <95090911+rawbytedev@users.noreply.github.com>
2025-10-25 07:35:32 +0100

    heavy updates

--- ShipmentProviderServer/main.py
+++ ShipmentProviderServer/main.py
@@ -3,34 +3,28 @@ Docstring for feedback_server
 This is the server used by the AI it provides feedback regarding shipments status
 in real use case the shipment provider sets it up to allow Ai to perform queries
 """
+from http.client import UNPROCESSABLE_CONTENT
 from urllib import request
 from fastapi import FastAPI, HTTPException, Request, Form
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
-from typing import Dict, List
+from typing import Dict, List, Union
 import datetime
 
+
 app = FastAPI(title="TrustMesh Feedback Server")
+# In-memory store for shipments
+shipments: Dict[str, Dict] = {} ## dictionary are mutable
+app.state.debugmode = True ## bool are not so we use app.state
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 class QueryRequest(BaseModel):
-    ids: list[str] | str
+    ids: Union[list[str], str]
 
 ## End of UI
-debugmode:bool = True
-# In-memory store for shipments
-shipments: Dict[str, Dict] = {}
-class ShipmentUpdate(BaseModel):
-    status: str = None   # created | in_transit | delayed | delivered | anomaly
-    location: str = None
-    notes: str = None
-
-class ShipmentDict(BaseModel):
-    ids: List[str] = []
-## this function allow to add or edit shipment status (demo)
 """
  {
  "shipment_id":"",
@@ -44,6 +38,21 @@ class ShipmentDict(BaseModel):
 """
 Take in a complete list of shipments id or a single shipment id
 """
+class CustomException(Exception):
+    def __init__(self, status_code: int, message: str):
+        self.status_code = status_code
+        self.message = message
+    def response(self):
+        if self.status_code == 422:
+            print("422 ERROR")
+
+@app.exception_handler(CustomException)
+async def exp(resquest: Request, exc: CustomException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"error": exc.message},
+    )
+
 @app.post("/query")
 def query_shipments(req: QueryRequest):
     ids = req.ids
@@ -58,41 +67,40 @@ def get_shipment_detail(ship_id:str):
     if ship_id in shipments:
         return {"id":ship_id, **shipments[ship_id]}
     else:
-        if debugmode:
+        if app.state.debugmode:
             shipments[ship_id] = {"id":ship_id, "status":"Debug", "location":"LocalHost","notes":"Debug",  "timestamp":timestamp()} 
             return shipments[ship_id]
-        return {"id":ship_id, "status":"Unknow", "notes":"not available", "location":"Unknow","timestamp":timestamp()}
+        return {"id":ship_id, "status":"Unknown", "notes":"not available", "location":"Unknown","timestamp":timestamp()}
 
 ## This is the entry point of App
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request":request, "shipments": shipments, "debug":debugmode})
+    return index({"request":request, "shipments": shipments, "debug":app.state.debugmode})
 
 ## Allow to add shipments details from dashboard
 @app.post("/add", response_class=HTMLResponse)
 def add_shipment(request: Request, id:str =Form(...), status:str=Form(...),location:str =Form(...),notes:str=Form("")):
     shipments[id]= {"status":status, "location":location, "notes":notes, "timestamp":timestamp()} 
-    return templates.TemplateResponse("index.html", {"request": request, "shipments":shipments, "debug":debugmode})
+    return index({"request": request, "shipments":shipments, "debug":app.state.debugmode})
 
-@app.post("/autoadd", response_class=HTMLResponse)
-def setautoadd(request: Request):
-    if debugmode:
-        print("accessing")
-        SetDebug(False)
-        return templates.TemplateResponse("index.html", {"request": request, "shipments":shipments, "debug":False})
-    print("seconds")
-    SetDebug(True)
-    return templates.TemplateResponse("index.html", {"request": request, "shipments":shipments, "debug":True})
-
-def SetDebug(boo:bool):
-    debugmode=boo
-    return
-
+@app.post("/toggle_autoadd", response_class=HTMLResponse)
+def toggle(request: Request):
+    if app.state.debugmode:
+        app.state.debugmode = False
+        return index({"request": request, "shipments":shipments, "debug":False})
+    app.state.debugmode = True
+    return index({"request": request, "shipments":shipments, "debug":True})
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
     #return {"status": "ok", "shipments_tracked": len(shipments)}
+
+# return to index using custom context
+def index(context: Dict | None = None):
+    if context is None:
+        context = {}
+    return templates.TemplateResponse("index.html", context=context)
 
 
 def timestamp():
--- ShipmentProviderServer/test.py
+++ ShipmentProviderServer/test.py
@@ -1,47 +1,23 @@
 import json
+import time
 import requests
 
 url = "http://127.0.0.1:8000"
 
-"""
-We only have 3 options
-/health - checks if server is up and running; also give a summary of how much items where tracked
-/shipment/id - output details related to a shipment using it's ID, with autodebug=True, create a new shipment if it doesn't exist and return it
-/shipment/id/update - can be used to both create new details, or update details of other shipments
-"""
-def selectShipment(action:int, id:int=0):
-    shipment_id = id
-    options ={
-    1:f"{url}/health",
-    2:f"{url}/shipment/{shipment_id}",
-    3:f"{url}/shipment/{shipment_id}/update",
-    4:f"{url}/shipments"
-    }
-    return options[action]
-
-def updateshipment(status, location, notes,url):
-    update = {
-        "status":status,
-        "location":location,
-        "notes":notes
-    }
-    payload = json.dumps(update) ## production needs a better encoder
-    re = requests.post(url, payload)
-    print(f"Updating Shipment")
-    print(f"Results: Status code: {re.status_code}")
-    out = json.loads(re.text)
-    print(f"shipment ID: {out["data"]["shipment_id"]}\nStatus: {out["data"]["status"]}\nlocation: {out["data"]["location"]}\nnotes: {out["data"]["notes"]}\ntimestamp: {out["data"]["timestamp"]}")
-
-
-def queryships():
-    ids = {"ids":["1", "2", "3"]}
+def queryships(single:bool=False):
+    if single:
+        ids = {"ids":"1"}
+    else:
+        ids = {"ids":["1", "2", "3", 1]}
+    before = time.time()
     if gethealth():
         payload = json.dumps(ids)
         res = requests.post(url+"/query", data=payload)
         result = json.loads(res.text)
         print(result)
         print("Query Results")
-        [print(f"id: {i["id"]}, status: {i["status"]}, notes:{i["notes"]}")for i in result["details"]]
+        [print(f"id: {i["id"]}, status: {i["status"]}, notes:{i["notes"]}, location:{i["location"]}")for i in result["details"]]
+        print(f"Took: {time.time()-before} to query")
         return True
     return False
 
@@ -52,6 +28,7 @@ def gethealth():
         if payload["status"] == "ok":
             return True
     return False
+
 
 if __name__ == "__main__":
     queryships()
