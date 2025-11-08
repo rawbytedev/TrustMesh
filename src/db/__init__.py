16c3a430199d19ca8737eadddab326f1c203310e
rawbytedev <95090911+rawbytedev@users.noreply.github.com>
2025-11-08 15:37:45 +0100

    added fixes

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,7 +1,8 @@
 import os
 
 backend = os.getenv("DB_BACKEND", "lmdb")
-
+if backend == "postgrestest":
+    from .db_postgres_test import DB, DBError
 if backend == "postgres":
     from .db_postgres import DB, DBError
 else:
